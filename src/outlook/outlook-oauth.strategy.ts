04e15f9286da30dcda517d1ca0307fd83e0d15c4
shakirullah41 <shakirullah41@users.noreply.github.com>
2024-07-09 15:05:35 +0500

    store outlook access and refresh token to user account

--- src/outlook/outlook-oauth.strategy.ts
+++ src/outlook/outlook-oauth.strategy.ts
@@ -1,15 +1,11 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Strategy } from 'passport-azure-ad-oauth2';
-import { EmailService } from '../email/email.service';
 import { OutlookService } from './outlook.service';
 
 @Injectable()
 export class OauthStrategy extends PassportStrategy(Strategy, 'azure-ad') {
-  constructor(
-    private readonly outlookService: OutlookService,
-    private readonly emailService: EmailService,
-  ) {
+  constructor(private readonly outlookService: OutlookService) {
     super(outlookService.getOAuthConfig());
   }
   authenticate(req, options) {
@@ -19,36 +15,27 @@ export class OauthStrategy extends PassportStrategy(Strategy, 'azure-ad') {
     }
     super.authenticate(req, options);
   }
-  async validate(
-    request,
-    accessToken,
-    refreshToken,
-    params,
-    profile,
-    done,
-  ): Promise<any> {
-    console.log(accessToken);
-    console.log(accessToken, refreshToken, params, profile, done);
+  async validate(request, accessToken, refreshToken): Promise<any> {
     try {
       const { state } = request.query;
       let userId;
       if (state) {
         userId = decodeURIComponent(state.toString());
-        console.log(userId);
       }
+      await this.outlookService.updateOutlookTokens(
+        userId,
+        accessToken,
+        refreshToken,
+      );
       const user = await this.outlookService.getUserProfile(accessToken);
-      //const user = await this.outlookService.validateUser(userProfile);
       if (!user) {
         throw new UnauthorizedException();
       }
-      user.outlookAccessToken = accessToken;
-      user.outlookRefreshToken = refreshToken;
       await this.outlookService.sync(user);
       return user;
     } catch (error) {
       console.log(error);
       return error;
-      // done(error, false);
     }
   }
 }
